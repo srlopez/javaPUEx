4ebee4977b35db1ba31df7deda725b0fe4fcbf36
Santiago R. López <slopez@plaiaundi.com>
2021-01-31 13:42:46 +0100

    Texto editado

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -8,19 +8,39 @@ public class Calculadora {
         return Math.floor(valor * 100) / 100d;
     }
 
+    // f1
+    // Devuelve el valor de una palabra
+    // Uuunoo, UNO, 1, UnnnO ->
+    // etc... hasta 9
     public int valorDeUnaPalabra(String text) {
         List<String> nombres = new ArrayList<>(
                 List.of("cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"));
 
-        String textNormalizado = text.toLowerCase().trim();
-        // https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
-        // https://stackoverflow.com/questions/106067/regular-expression-to-replace-two-or-more-consecutive-characters-by-only-one
-        // reemplaza cualquier carácter que aparece más de una vez, por él mismo.
-        textNormalizado = textNormalizado.replaceAll("(.)\\1+", "$1");
-        int idx = nombres.indexOf(textNormalizado);
-        return idx < 0 ? 0 : idx;
+        int valor = 0;
+        String textNormalizado = text.trim().toLowerCase();
+        // Verificamos si es un numero entero
+        try {
+            valor = Integer.parseInt(textNormalizado);
+            valor = valor > 9 ? 0 : valor;
+        } catch (Exception e) {
+            valor = 0;
+        }
+        if (valor == 0) {
+            // verificamos que es un nombre reconocido
+            // https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
+            // https://stackoverflow.com/questions/106067/regular-expression-to-replace-two-or-more-consecutive-characters-by-only-one
+            // reemplaza cualquier carácter que aparece más de una vez, por él mismo.
+            textNormalizado = textNormalizado.replaceAll("(.)\\1+", "$1");
+            valor = nombres.indexOf(textNormalizado);
+            valor = valor < 0 ? 0 : valor;
+        }
+        return valor;
     }
 
+    // f2: invoca a f1
+    // Devuelve una Lista de Enteros
+    // Obtenida por valorar cada palabra en un String
+    // Unnooo dos 3 fin -> 1,2,3,0
     public List<Integer> valoresDeVariasPalabras(String text) {
         List<Integer> valores = new ArrayList<Integer>();
 
@@ -32,6 +52,9 @@ public class Calculadora {
         return valores;
     }
 
+    // f3
+    // Devuelve una Lista de valores dobles proporcionales a
+    // Una Lista de Enteros y un valor Total entero
     public List<Double> valoresProporcionalesDeUnaListaInteger(List<Integer> valores, int total) {
         List<Double> proporciones = new ArrayList<Double>();
 
@@ -47,6 +70,9 @@ public class Calculadora {
         return proporciones;
     }
 
+    // f4: invoca a f3 y f2
+    // Devuelve una Lista de valores dobles proporcionales a
+    // Una Lista de Enteros obtenida por valorar cada palabra en un String, y un valor Total entero
     public List<Double> valoresProporcionalesDeVariasPalabras(String texto, int total) {
         List<Integer> valores = valoresDeVariasPalabras(texto);
         List<Double> proporciones = valoresProporcionalesDeUnaListaInteger(valores, total);
--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculadoraTest {
@@ -14,7 +15,7 @@ public class CalculadoraTest {
     // @Test
     @ParameterizedTest
     @DisplayName("UNO->1")
-    @ValueSource(strings = { " uunNOo "," uNo", "UNO ", "  uNO  " })
+    @ValueSource(strings = { " uunNOo "," uNo", "UNO ", "  uNO  ", " 1 " })
     void deTextoAValor1(String txt) {
         // 1 - preracion del test
         Calculadora calc = new Calculadora();
@@ -27,27 +28,27 @@ public class CalculadoraTest {
     //Similar para DOS, TRES, etc...
 
     @Test
-    @DisplayName("De Texto a Lista de valores1")
+    @DisplayName("De Texto a Lista de valores #1")
     void deTextoAValores1() {
         // 1 - preracion del test
         Calculadora calc = new Calculadora();
         // 2 - Ejecutar lo que vamos a validar
-        String texto = "Hey uNo dOs sEIs santi";
+        String texto = " Hey  uNo  dOs  sEIs 3 santi ";
         List<Integer> result = calc.valoresDeVariasPalabras(texto);
         // 3 - Aserciones
         List<Integer> valoresEsperados = new
-        ArrayList<Integer>(Arrays.asList(0,1,2,6,0));
+        ArrayList<Integer>(Arrays.asList(0,1,2,6,3,0));
         assertEquals(valoresEsperados, result);
     }
 
     @Test
-    @DisplayName("De Lista de valores y total a Proporciones1")
+    @DisplayName("De Lista de valores y total a Proporciones #1")
     void deValoresAProporciones1() {
         // 1 - preracion del test
         Calculadora calc = new Calculadora();
         // 2 - Ejecutar lo que vamos a validar
-        List<Integer> valores = new ArrayList<Integer>(Arrays.asList(1, 2, 6));
-        int total = 6;
+        List<Integer> valores = new ArrayList<Integer>(Arrays.asList(0, 7, 9, 3, 5));
+        int total = 660;
         List<Double> result = calc.valoresProporcionalesDeUnaListaInteger(valores, total);
         // 3 - Aserciones
         List<Double> valoresEsperados = new ArrayList<Double>(Arrays.asList(0.66, 1.33, 4.0));
@@ -55,7 +56,7 @@ public class CalculadoraTest {
     }
 
     @Test
-    @DisplayName("De Lista de valores INT y total a Proporciones2 DOUBLES")
+    @DisplayName("De Lista de valores y total a Proporciones #2")
     void deValoresAProporciones2() {
         // 1 - preracion del test
         Calculadora calc = new Calculadora();
@@ -71,12 +72,12 @@ public class CalculadoraTest {
 
 
     @Test
-    @DisplayName("De Textos y total a Proporciones1")
-    void deTextosAProporciones() {
+    @DisplayName("De Textos y total a Proporciones #1")
+    void deTextosAProporciones1() {
         // 1 - preracion del test
         Calculadora calc = new Calculadora();
         // 2 - Ejecutar lo que vamos a validar
-        String texto ="  DOS  Uno  sEIs  ";
+        String texto ="  2  Uno  sEIs  ";
         int total = 60;
         List<Double> result = calc.valoresProporcionalesDeVariasPalabras(texto,total);
         // 3 - Aserciones
@@ -84,4 +85,31 @@ public class CalculadoraTest {
 
         assertEquals(valoresEsperados, result);
     }
+
+    
+    @DisplayName("Parameterized Test con CSV")
+    @ParameterizedTest(name = "run #{index}: '{0} -> {1}€/{2} monedas")
+    // "String de textos, total, String del array resultado"
+    @CsvSource (value = {
+        "  1DAW3  SiEtE  nueeVVE  3 cinco : 660:  0, 192.5, 247.5, 82.5, 137.5",
+        "  1 2 3 4: 100:  10, 20, 30, 40",
+        "  unnoooo  dos 3 CUATRO fin  : 15:  1.5, 3.0, 4.5, 6.0, 0.0",
+        "  UuunnnOOO  DOs  SSeiSS  : 60:  6.66, 13.33, 40.0 ",
+        "  Cerooo  0  1  99: 60:  0, 0, 60, 0 ",
+            }, delimiter = ':')
+    void testEfectivoStringCSV(String  texto, int total, String doblesCSV) {
+        Calculadora calc = new Calculadora();
+        //
+        String[] sdobles = doblesCSV.replaceAll("\\s+", "").split(",");
+        List<Double> valoresEsperados = new ArrayList<Double>();
+        for (int i = 0; i<sdobles.length; i++) valoresEsperados.add(Double.valueOf(sdobles[i]));
+        //
+        List<Double> result = calc.valoresProporcionalesDeVariasPalabras(texto,total);
+        //
+        assertEquals(valoresEsperados, result, valoresEsperados + "<-'"+texto+"'");
+    }
+
+
+
+
 }
